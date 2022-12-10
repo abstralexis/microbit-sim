dc58cb61e07002b99281aef2ab071aa785453332
AlexisComix <thecomixguy@hotmail.com>
2022-12-10 14:10:47 +0000

    Smily face drawing demo

--- src/microbit-sim.py
+++ src/microbit-sim.py
@@ -10,41 +10,57 @@ COLOURS = (
 )
 
 class Microbit:
+    """
+    Microbit simulation class with API
+    """
     def __init__(self):
+        """
+        Initialise Microbit simulator
+        """
         pygame.init()
         self.WIN = pygame.display.set_mode((250,250))
         pygame.display.set_caption("MicroBit Simulator")
-        self.px_array = numpy.zeros((5, 5))
-        
-        self.main()
+        self.led_array = [
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0],
+            [0,0,0,0,0]
+        ]
 
-    def main(self):
-        running = True
-        while running:
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    running = False
-                    pygame.quit()
-                    sys.exit()
-
-            self.draw()
+    def events(self):
+        """
+        Main
+        """
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                pygame.quit()
+                sys.exit()
 
     def draw(self):
+        """
+        Draw pixel array
+        """
         self.WIN.fill(BLACK)
         for i in range(5):
             for j in range(5):
-                px_rect = pygame.rect.Rect(
-                    i*50, 
-                    j*50,
-                    50,
-                    50
-                )
-                pygame.draw.rect(
-                    self.WIN,
-                    RED,
-                    px_rect,
-                )
+                led = pygame.Surface((50, 50))
+                led.fill(RED)
+                led.set_alpha((255/9) * self.led_array[j][i])
+                self.WIN.blit(led, (i*50, j*50))
         pygame.display.flip()
     
 if __name__ == "__main__":
-    Microbit()
+    mb = Microbit()
+    running = True
+    while running:
+        mb.led_array = [
+            [0,9,0,9,0],
+            [0,9,0,9,0],
+            [0,0,3,0,0],
+            [9,0,3,0,9],
+            [0,9,9,9,0]
+        ]
+
+        mb.events()
+        mb.draw()
